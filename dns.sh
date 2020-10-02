ad8bb1dd7aed095e9bf9d1e5289100b318cc6158
baileywickham <baileywickham@gmail.com>
2020-10-02 16:53:37 -0700

    update dns script

--- dns.sh
+++ dns.sh
@@ -1,6 +1,6 @@
 if ! (( ip a | grep tun0 > /dev/null )) then
-    dig +short myip.opendns.com @resolver1.opendns.com > ip
+    dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \" > ip
     now=$(date)
-    git commit ip -m 'update ip on ${now}' > /dev/null
-    git push > /dev/null
+    git commit ip -m "update ip on ${now}"
+    git push
 fi
